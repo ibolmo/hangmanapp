7b65197c9be8851c2b59dbb37a55738fb635e88e
Olmo Maldonado <olmo.maldonado@gmail.com>
2013-11-25 23:08:46 -0600

    Adde analytics

--- client/client.js
+++ client/client.js
@@ -124,6 +124,16 @@ Template.login.show = function(){
   return !!Meteor.user();
 };
 
+Template.analytics.created = function(){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45986467-1', 'meteor.com');
+  ga('send', 'pageview');
+};
+
 Meteor.setInterval(function () {
   Meteor.call('keepalive', Meteor.userId());
 }, 5000);
